3b84c6f47ba5ffee1be2b7b19f90ebd88205d535
neil.richards <nedster1980@gmail.com>
2018-12-15 16:37:58 +0000

    Working file to deploy a cluster of web servers

--- main.tf
+++ main.tf
@@ -5,10 +5,13 @@ variable "server_port" {
 }
 
 #Display public-ip without pocking around in AWS console
+/*
 output "public_ip" {
   value = "${aws_instance.example.public_ip}"
 }
+*/
 
+data "aws_availability_zones" "all" {}
 
 provider "aws" {
   region = "eu-west-2"
@@ -18,7 +21,7 @@ provider "aws" {
 
 
 resource "aws_security_group" "instance"{
-  name = "terraform-example-instance"
+  name = "terraform-example-instance2"
 
   ingress {
     from_port = "${var.server_port}"
@@ -26,10 +29,13 @@ resource "aws_security_group" "instance"{
     to_port = "${var.server_port}"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
-
-
+/*
 resource "aws_instance" "example" {
   ami = "ami-0f60b09eab2ef8366"
   instance_type = "t2.micro"
@@ -48,4 +54,35 @@ resource "aws_instance" "example" {
   }
 
 }
+*/
+
+resource "aws_launch_configuration" "example" {
+  image_id = "ami-0f60b09eab2ef8366"
+  instance_type = "t2.micro"
+  security_groups = ["${aws_security_group.instance.id}"]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p "${var.server_port}" &
+              EOF
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = "${aws_launch_configuration.example.id}"
+  availability_zones = ["${data.aws_availability_zones.all.names}"]
+
+  max_size = 10
+  min_size = 2
+  tag {
+    key = "Name"
+    propagate_at_launch = true
+    value = "terraform-asg-example"
+  }
+}
+
 
